be4c5c4210da4512cb2f8804a467d750ae9d81a1
Gaveesha Welhena <gaveeshawelhena@gmail.com>
2026-04-19 17:25:51 +0530

    connected backend in render

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://issue-tracker-backend-8mqf.onrender.com",
 });
 
 api.interceptors.request.use((config: any) => {
